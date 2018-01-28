da3491da88e202deffb50eb0a19b78e7dfc6e228
SergeyPonomarenko92 <35901988+SergeyPonomarenko92@users.noreply.github.com>
2018-01-28 23:27:37 +0200

    Added queue implementation

---
+++ Queue.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class Item
+        {
+           public int value;
+            public Item next;
+
+            public Item(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+
+        }
+        public class Queue
+        {
+            Item head;
+            Item tail;
+
+            // constructor
+            public Queue() { }
+
+            public void Enqueue(int value)
+            {
+                Item it = new Item(value);
+                if (head == null)
+                {
+                    head = it;
+                    tail = it;
+                }
+                else
+                {
+                    tail.next = it;
+                    tail = it;
+                }
+            }
+             public int Dequeue () {
+                if (head == null)
+                {
+                    Exception e = new Exception();
+                    throw e;
+                }
+                else {
+                    int value = head.value;
+                    head = head.next;
+                    return value;
+                }
+
+            }
+            public int Peek() {
+                if (head == null)
+                {
+                    Exception a = new Exception();
+                    throw a;
+                }
+                else {
+                    return head.value;
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            for (int i = 0; i <= 10; i++) {
+                q.Enqueue(i);
+            }
+            Console.WriteLine(q.Peek());
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Peek());
+            Console.WriteLine(q.Peek());
+            Console.WriteLine(q.Peek());
+
+        }
+    }
+}
